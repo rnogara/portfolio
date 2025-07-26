3e4666e0ab023b8873f240602abacda86b16b080
rnogara <r.nogara.dev@gmail.com>
2025-07-26 16:53:07 -0300

    fix: change to build backend and frontend separately

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
-    unoptimized: true,
     domains: ['img.icons8.com', 'live.staticflickr.com', 'svgl.app'],
     remotePatterns: [
       {
